75eb8820b1de17734e3f58e40b5b06946c3d5b3a
bhavankumar2911 <bhavankumarcse2020@gmail.com>
2022-12-28 15:50:31 +0530

    website colors changed responsive login page created

--- client/components/utils/Logo/index.tsx
+++ client/components/utils/Logo/index.tsx
@@ -6,9 +6,9 @@ const Logo: React.FC<Props> = ({ white }) => {
   return (
     <div>
       <Link href="/">
-        <h1 className="text-3xl font-bold uppercase">
-          <span className={white ? "text-white" : "text-black"}>true</span>
-          <span className="text-red-600">skilled</span>
+        <h1 className="text-3xl font-bold">
+          <span className={white ? "text-white" : "text-indigo-900"}>True</span>
+          <span className="text-green-500">Skilled</span>
         </h1>
       </Link>
     </div>
--- client/pages/login/index.tsx
+++ client/pages/login/index.tsx
@@ -1,25 +1,34 @@
+import Link from "next/link";
 import React from "react";
 import Logo from "../../components/utils/Logo";
 
 const Login = () => {
   return (
-    <main>
+    <main className="sm:grid sm:grid-cols-3">
       {/* top */}
-      <section className="bg-[url(/auth.jpg)] min-h-[200px] bg-no-repeat bg-center bg-cover relative after:content-[''] after:bg-black after:absolute after:inset-0 after:opacity-80 after:z-10 flex items-center justify-center">
-        <div className="relative z-20">
+      <section className="bg-[url(/auth.jpg)] min-h-[200px] bg-no-repeat bg-center bg-cover relative after:content-[''] after:bg-black after:absolute after:inset-0 after:opacity-80 after:z-10 flex items-center justify-center sm:after:content-none sm:h-screen">
+        <div className="relative flex items-center z-20 sm:hidden">
           <Logo white={true} />
-          <h2 className="text-white text-xl uppercase mt-3">login</h2>
+          <span className="bg-white block h-10 w-[1px] mx-3"></span>
+          <h2 className="text-white text-xl capitalize">login</h2>
         </div>
       </section>
 
       {/* bottom */}
-      <section>
-        <form className="w-3/4 mx-auto mt-10">
+      <section className="sm:col-span-2 sm:h-screen sm:flex sm:items-center">
+        <form className="w-3/4 mx-auto mt-10 max-w-[384px] sm:mt-0">
+          <div className="hidden sm:items-center sm:flex mb-10">
+            <Logo />
+            <span className="bg-gray-800 block h-10 w-[1px] mx-5"></span>
+            <h2 className="text-gray-800 text-xl capitalize font-semibold">
+              login
+            </h2>
+          </div>
           <input
             type="email"
             name="email"
             id="email"
-            className="border rounded-sm w-full hover:border-red-600 outline-none p-3"
+            className="border rounded-sm w-full hover:border-gray-400 outline-none p-2"
             placeholder="Email"
           />
           <br />
@@ -28,16 +37,20 @@ const Login = () => {
             type="password"
             name="password"
             id="password"
-            className="border rounded-sm w-full hover:border-red-600 outline-none p-3"
+            className="border rounded-sm w-full hover:border-gray-400 outline-none p-2"
             placeholder="Password"
           />
           <br />
           <br />
-          <input
+          <button
+            className="w-full rounded-sm capitalize px-4 py-2 text-sm font-semibold bg-green-500 text-white hover:bg-indigo-900 transition"
             type="submit"
-            value="log in"
-            className="w-full rounded-sm uppercase px-4 py-3 bg-red-600 text-white"
-          />
+          >
+            log in
+          </button>
+          <p className="text-center text-sm mt-5 text-gray-500">
+            New user? <Link href="/">Sign Up</Link>
+          </p>
         </form>
       </section>
     </main>
